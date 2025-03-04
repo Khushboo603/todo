4aa56f216f4675c35172babda09688d50c96d809
khushboo <rishikasinha786@gmail.com>
2025-03-04 15:48:42 +0530

    post get delete api done

--- todoDrf/todoDrf/urls.py
+++ todoDrf/todoDrf/urls.py
@@ -19,7 +19,8 @@ from django.urls import path
 from todo import views
 
 urlpatterns = [
-    path('', views.index, name="todo"),
+    # path('', views.index, name="todo"),
+    path('api/todos/', views.index, name="todo"),
     path('del/<int:item_id>', views.remove, name="del"),
     path('admin/', admin.site.urls),
 ]

9731c7296fd486cd5fdd27676c51080e2d5ead68
khushboo <rishikasinha786@gmail.com>
2025-03-04 11:55:35 +0530

    Get data and post Data Completed

--- todoDrf/todoDrf/settings.py
+++ todoDrf/todoDrf/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,13 +51,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'todoDrf.urls'
 
 # Allow all domains to access your API (in development)
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ORIGIN_WHITELIST = ['http://localhost:8000']
 
 TEMPLATES = [
     {
--- todoDrf/todoDrf/urls.py
+++ todoDrf/todoDrf/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from todo import views
 
 urlpatterns = [
-    path('', views.index, name="todo"),
+    path('api/todos/', views.index, name="todo"),
     path('del/<int:item_id>', views.remove, name="del"),
     path('admin/', admin.site.urls),
+    # path('api/todo/', include('todo.urls')),
 ]
